61662cf65e661b9dcf47bc128b5eaf09ef46c1fa
Eduard <e.keilholz@4dotnet.nl>
2022-11-09 11:33:34 +0100

    retry

--- src/PollStar.ReverseProxy.App/Proxy/PollStarProxyConfigProvider.cs
+++ src/PollStar.ReverseProxy.App/Proxy/PollStarProxyConfigProvider.cs
@@ -2,57 +2,44 @@
 using Yarp.ReverseProxy.LoadBalancing;
 using Yarp.ReverseProxy.Transforms;
 
-namespace PollStar.ReverseProxy.App.Proxy
+namespace PollStar.ReverseProxy.App.Proxy;
+
+public class PollStarProxyConfigProvider : IProxyConfigProvider
 {
-    public class PollStarProxyConfigProvider : IProxyConfigProvider
+    private readonly PollStarMemoryConfig _config;
+    public IProxyConfig GetConfig() => _config;
+
+    public PollStarProxyConfigProvider()
     {
-        private PollStarMemoryConfig _config;
-
-        public IProxyConfig GetConfig() => _config;
-
-        public PollStarProxyConfigProvider()
+        var routeConfigs = new[] {GetUsersRoute()};
+        var clusterConfigs = new[]
         {
-            // Load a basic configuration
-            // Should be based on your application needs.
-
-
-            var routeConfigs = new[] { GetUsersRoute() };
-
-            var clusterConfigs = new[]
+            new ClusterConfig
             {
-                new ClusterConfig
-                {
-                    ClusterId = "users-service",
-                    LoadBalancingPolicy = LoadBalancingPolicies.RoundRobin,
-                    Destinations = new Dictionary<string, DestinationConfig>
-                    {
-                        { "destination1", new DestinationConfig { Address = "http://localhost:80" } },
-                    }
-                }
-            };
-
-            _config = new PollStarMemoryConfig(routeConfigs, clusterConfigs);
-        }
-
-        private RouteConfig GetUsersRoute()
-        {
-            var route = new RouteConfig
-            {
-                RouteId = "users-route",
                 ClusterId = "users-service",
-                CorsPolicy = Constants.DefaultCorsPolicy,
-                Match = new RouteMatch
+                LoadBalancingPolicy = LoadBalancingPolicies.RoundRobin,
+                Destinations = new Dictionary<string, DestinationConfig>
                 {
-                    Path = "/users/{**catch-all}"
-                },
-            };
-            return route
-                .WithTransformPathPrefix("/api")
-                .WithTransformRequestHeader("dapr-app-id", "pollstar-users-api");
+                    {"destination1", new DestinationConfig {Address = "http://localhost:80/v1.0/invoke/users-service/method/api/"}},
+                }
+            }
+        };
+        _config = new PollStarMemoryConfig(routeConfigs, clusterConfigs);
+    }
 
-        }
-
-
-
+    private RouteConfig GetUsersRoute()
+    {
+        var route = new RouteConfig
+        {
+            RouteId = "users-route",
+            ClusterId = "users-service",
+            CorsPolicy = Constants.DefaultCorsPolicy,
+            Match = new RouteMatch
+            {
+                Path = "/users/{**catch-all}"
+            },
+        };
+        return route
+            .WithTransformRequestHeader("dapr-app-id", "pollstar-users-api");
     }
 }
